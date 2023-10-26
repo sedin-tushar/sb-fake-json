b5c5a7031cf5b33caa66e9dae66dd88c7d071f38
mithun30052001 <lmithun01@gmail.com>
2023-10-26 10:14:25 +0530

    UserTypes mock json added and published as v1.0.2

---
+++ Ed-portal/feature/onboarding-flow/userType/UserTypeMock.js
@@ -0,0 +1,81 @@
+[
+    {
+        "code": "teacher",
+        "name": "Teacher",
+        "label": "frmelmnts.lbl.teacher",
+        "visibility": true,
+        "image": "guest-img1.svg",
+        "index": 0,
+        "searchFilter": [
+            "Teacher",
+            "Instructor"
+        ]
+    },
+    {
+        "code": "student",
+        "name": "Student",
+        "visibility": true,
+        "label": "frmelmnts.lbl.student",
+        "image": "guest-img2.svg",
+        "index": 1,
+        "searchFilter": [
+            "Student",
+            "Learner"
+        ]
+    },
+    {
+        "code": "administrator",
+        "name": "School head OR Officials",
+        "index": 3,
+        "visibility": true,
+        "label": "frmelmnts.lbl.administrator",
+        "image": "guest-img5.svg",
+        "searchFilter": [
+            "administrator"
+        ]
+    },
+    {
+        "code": "parent",
+        "name": "Parent",
+        "index": 2,
+        "visibility": true,
+        "label": "frmelmnts.lbl.parent",
+        "image": "guest-img4.svg",
+        "searchFilter": [
+            "Student",
+            "Teacher",
+            "Instructor",
+            "Learner"
+        ]
+    },
+
+    {
+        "code": "farmer",
+        "name": "Farmer",
+        "index": 4,
+        "visibility": true,
+        "label": "Farmer",
+        "image": "guest-img4.svg",
+        "searchFilter": [
+            "Farmer",
+            "Crops",
+            "Instructor",
+            "Learner"
+        ]
+    },
+
+    {
+        "code": "other",
+        "name": "Other",
+        "visibility": false,
+        "index": 5,
+        "label": "frmelmnts.lbl.other",
+        "image": "guest-img3.svg",
+        "searchFilter": [
+            "Student",
+            "Teacher",
+            "Instructor",
+            "Learner"
+        ]
+    },
+]
