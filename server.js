a575669cde2e38e6ae5c9496311be266e1096d21
mithun30052001 <lmithun01@gmail.com>
2023-10-26 17:50:37 +0530

    Switch case added to send the corresponding mock json based on key

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 //mock name_component name
-const locationMockData = require('./Ed-portal/feature/onboarding-flow/location/LocationMock');
+const locationMockData_locationdelegate = require('./Ed-portal/feature/onboarding-flow/location/LocationMock');
 const courseMockData_explorecontent = require('./Ed-portal/feature/LandingPage/AllCourses/MockCourseData');
 const responseMockData_explorepage = require('./Ed-portal/feature/LandingPage/Home/MockResponse');
-const userRolesMockData = require('./Ed-portal/feature/onboarding-flow/userType/UserRolesMock')
+const userRolesMockData_locationdelegate = require('./Ed-portal/feature/onboarding-flow/location/UserRolesMock')
+const userTypesMockData_onboardinguser = require('./Ed-portal/feature/onboarding-flow/userType/UserTypeMock')
+
 
 module.exports = {
-  // key refers to the component name to be called
-  getMockResponseData: (key) => {
-    if (key === 'explore-page') {
-      return responseMockData_explorepage;
-    }
-    else if(key === 'explore-content'){
-      return courseMockData_explorecontent;
+  getMockResponseData: function (key) {
+    switch (key) {
+      case 'delegate-location':
+        return locationMockData_locationdelegate;
+      case 'delegate-role':
+        return userRolesMockData_locationdelegate;
+      case 'explore-page':
+        return responseMockData_explorepage;
+      case 'explore-content':
+        return courseMockData_explorecontent;
+      case 'onboarding-user':
+        return userTypesMockData_onboardinguser;
+      default:
+        return null;
     }
   },
 };
-
-
